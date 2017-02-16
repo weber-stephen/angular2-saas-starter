06dee9fd275080b30510db0afd19786775c056c0
Stephen Weber <me@stephenweber.io>
2017-02-15 22:21:23 -0800

    Reorganized the SCSS files to properly override the bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
 import { NavigationComponent } from '../navigation/navigation.component';
+import { ToolbarComponent } from '../toolbar/toolbar.component';
 
-import { AuthenticationService } from '../services/authentication.service'
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-root',
